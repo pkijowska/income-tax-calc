5d7631d2370263c31078623f14716ae104ef34a1
paulina <p.kijowska@gmail.com>
2021-09-17 13:33:37 +1000

    income tax calc finish

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const FREQUENCIES = {yearly: 1, quarterly: 4, monthly: 12, fortnightly: 26.09, weekly: 52.18};
+const FREQUENCIES = { yearly: 1, quarterly: 4, monthly: 12, fortnightly: 26.09, weekly: 52.18 };
 
 function formatThousands(num) {
     var sep = ",";
@@ -11,72 +11,56 @@ function formatThousands(num) {
     x2 = x.length > 1 ? '.' + x[1] : '';
     var rgx = /(\d+)(\d{3})/;
     while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
     }
     return x1 + x2;
 }
 
-var deductions = function(income, incomeRanges, rates) {
-    let finalTax = 0;
-    let findCurrentIncome = incomeRanges.filter(i => i < income);
-    let findLastElementInIncomeRange = findCurrentIncome.slice(-1)[0];
-    let calcHighestTax = (income-findLastElementInIncomeRange) * rates[incomeRanges.indexOf(findLastElementInIncomeRange)];
-    finalTax += calcHighestTax;
-    findCurrentIncome.reverse().forEach(function(i){
-        const indexIncome = incomeRanges.indexOf(i);
-        if (indexIncome >0) {
-            const individualTax= (i -incomeRanges[indexIncome-1]) * rates[indexIncome-1];
-            finalTax += individualTax;
+
+function calc(income, incomeRanges, deductions) {
+
+    let tax = 0;
+
+    for (let i = 0; i < incomeRanges.length; i++) {
+        if (income < incomeRanges[i]) {
+            continue;
         }
-    });
 
-    return finalTax
-}
+        tax += (income - incomeRanges[i]) * deductions[i];
 
-var calcIncomeTax = function(income) {
-   return deductions(income, [0, 12570, 50270, 150000], [0, 0.2, 0.4, 0.45])
-}
-
-var calcNationalInsurance = function(income) {
-   return deductions(income, [0, 9564, 50268], [0, 0.12, 0.02])
+        income = incomeRanges[i];
+    }
+    return tax
 }
 
 
-// function test(income, tax) {
-//     console.log(`when income is ${income}, tax is ${tax}`)
-//     if (deductions(income, [0, 12570, 50270, 150000], [0, 0.2, 0.4, 0.45]) == tax) {
-//         console.log("success");
-//     }
-//     else {
-//         console.log("fail");
-    
-//     }
-// }
+var calcIncomeTax = function (income) {
+    return calc(income, [150001, 50271, 12571, 0], [0.45, 0.4, 0.2, 0])
+}
 
-// // test(0,0);
-// // test(12570,0);
-// // test(50270, 7540);
-// test(200000, 69931.40)
+var calcNationalInsurance = function (income) {
+    return calc(income, [50269, 9564, 0], [0.02, 0.12, 0])
+}
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('#income').inputmask().keyup(calculateTax);
     $('#income').val(0).trigger('keypress');
 });
 
 
 function calculateTax() {
-    $(this).on('keypress', function(e) {
+    $(this).on('keypress', function (e) {
         const ESCAPE_KEY_CODE = 13
         if (e.keyCode == ESCAPE_KEY_CODE) {
             event.preventDefault();
         }
     })
 
-    var income = parseInt($('#income').inputmask('unmaskedvalue')) 
+    var income = parseInt($('#income').inputmask('unmaskedvalue'))
 
     let totalTax = calcIncomeTax(income) + calcNationalInsurance(income);
     const net = income - totalTax;
-    
+
 
     var ftax = '£' + (isNaN(Math.round(net)) ? '0' : formatThousands(Math.round(calcIncomeTax(income))));
     var fni = '£' + (isNaN(Math.round(calcNationalInsurance(income))) ? '0' : formatThousands(Math.round(calcNationalInsurance(income))));
